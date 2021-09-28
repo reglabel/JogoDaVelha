f75434f28ec4c1241f1a2cab58120b60ddaab67b
reglabel <regla.belcl@gmail.com>
2021-09-28 10:08:14 -0300

    Updating repository

--- Facul/BlackJack/src/game/sistema/excecoes/CodigoInvalido.java
+++
@@ -1,9 +0,0 @@
-package game.sistema.excecoes;
-
-public class CodigoInvalido extends Exception{
-	private static final long serialVersionUID = 1L;
-	
-	public CodigoInvalido() {
-	    super("-> Código indisponível! Tente novamente.");
-	}
-}
--- Facul/BlackJack/src/game/sistema/excecoes/NomeInvalido.java
+++
@@ -1,9 +0,0 @@
-package game.sistema.excecoes;
-
-public class NomeInvalido extends Exception{
-	private static final long serialVersionUID = 1L;
-	
-	public NomeInvalido() {
-	    super("-> Nome indisponível! Tente novamente.");
-	}
-}
